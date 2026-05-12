4752096a85307ac37d7775e749973d3e278b39ed
Davi <davi.waitman@gmail.com>
2026-05-12 17:58:13 -0300

    Navegação entre telas e navbar

---
+++ NavBar/Navbar.php
@@ -0,0 +1,80 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Titan Sports</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<nav class="navbar-ml">
+
+  <div class="container-navbar">
+
+    <a href="#" class="logo">
+      <img src="imagens/Logo-sem-fundo.png" alt="Logo">
+    </a>
+
+    <form class="search-box">
+      <input type="search" placeholder="Buscar produtos, marcas e muito mais...">
+      <button type="submit">
+        <i class="fas fa-search"></i>
+      </button>
+    </form>
+
+    <div class="user-area">
+      <a href="login.html" class="profile-box">
+        <img src="imagens/Perfil.png" class="profile-img" alt="Perfil">
+        <div class="profile-info">
+          <span class="welcome">Entrar</span>
+          <span class="status">Minha conta</span>
+        </div>
+
+      </a>
+
+    </div>
+
+  </div>
+
+  <div class="menu-bar">
+
+    <div class="dropdown">
+
+      <button class="dropdown-btn">
+        Categorias ▼
+      </button>
+
+      <div class="dropdown-content">
+        <a href="#">Futebol</a>
+        <a href="#">Basquete</a>
+        <a href="#">Corrida</a>
+        <a href="#">Academia</a>
+        <a href="#">Natação</a>
+        <a href="#">Suplementos</a>
+        <a href="#">Camisas de Times</a>
+        <a href="#">Acessórios</a>
+      </div>
+
+    </div>
+
+    <a href="#">Ofertas</a>
+    <a href="#">Masculino</a>
+    <a href="#">Feminino</a>
+    <a href="#">Lançamentos</a>
+
+  </div>
+</nav>
+</body>
+</html>
